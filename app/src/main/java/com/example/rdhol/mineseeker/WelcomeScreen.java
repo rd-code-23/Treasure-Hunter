b3118186ea71c2f79d780250648faf0aede739c3
rdhol <rdholliwar@hotmail.com>
2017-02-25 15:35:17 -0800

    added main menu design

--- app/src/main/java/com/example/rdhol/mineseeker/WelcomeScreen.java
+++ app/src/main/java/com/example/rdhol/mineseeker/WelcomeScreen.java
@@ -13,7 +13,7 @@ public class WelcomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
-     
+
         helmAnim();
         setupMainMenuButton();
     }
